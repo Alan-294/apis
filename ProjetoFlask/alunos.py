3c920107a568aff1d9f8d539b91f71a674f4b59d
Fernanda Figueiredo <fernanda.figueiredo@aluno.faculdadeimpacta.com.br>
2025-03-16 16:18:38 -0300

    Altera arquivo alunos.py

--- ProjetoFlask/alunos.py
+++ ProjetoFlask/alunos.py
@@ -21,30 +21,17 @@ alunos_db = [
 #Caminho do método GET
 #http://127.0.0.1:5000/api/alunos
 
-def aluno():
+def cria_aluno():
     if request.method == 'POST':
-      
-        nome = request.args.get('nome', type=str)
-        data = request.args.get('data', type=str)
-        nota_primeiro_semestre = request.args.get('nota_primeiro_semestre', type=int)
-        nota_segundo_semestre = request.args.get('nota_segundo_semestre', type=int)
-        media_final = (nota_primeiro_semestre + nota_segundo_semestre) /2
-
-        aluno = {
-            "turma_id": 6,
-            "nome": nome,
-            "data_nascimento": data,
-            "nota_primeiro_semestre": nota_primeiro_semestre,
-            "nota_segundo_semestre": nota_segundo_semestre,
-            "media_final": media_final
-            }
+        aluno = request.get_json()
+        mediaFinal = (aluno['nota_primeiro_semestre'] + aluno['nota_segundo_semestre']) / 2
+        aluno['media_final'] = mediaFinal
         alunos_db.append(aluno)
-
         return jsonify(aluno)
-
+    
     elif request.method == 'GET':
         return jsonify(alunos_db)
-        
+
 
 if __name__ == '__main__':
     app.run(debug=True)
