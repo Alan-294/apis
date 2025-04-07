4a7c37559ccc9e7f41bbdebc74b86473dd996067
lirouana <ana.misquita@aluno.faculdade.com.br>
2025-04-07 16:41:56 -0300

    update professores.py

--- ProjetoFlask/professores.py
+++ ProjetoFlask/professores.py
@@ -1,16 +1,35 @@
 from flask import Flask,jsonify, request
 from app import app
-from BD import professores_db, turma_db
+import model_professores as model
 
+professores_db = [
+    {
+        "id": 1,
+        "nome": "Maria",
+        "data_nascimento": "2003-02-03",
+        "disciplina": "Math",
+        "salario": 1500,
+        "Observações": "None"
+    },
+    {
+        "id": 2,
+        "nome": "Joao",
+        "data_nascimento": "2005-10-03",
+        "disciplina": "Chemistry",
+        "salario": 2500,
+        "Observações": "None"
+    },
+    {
+        "id": 3,
+        "nome": "Pedro",
+        "data_nascimento": "2005-10-03",
+        "disciplina": "English",
+        "salario": 1550,
+        "Observações": "None"
+    }
+    
+]
 
-def acharTurmas(professor_id):
-    turmas = []
-
-    for turma in turma_db:
-        if (turma["professor_id"] == professor_id):
-            turmas.append(turma)
-            
-    return turmas
 @app.route('/api/professores', methods=['GET'])
 def professores():
     return jsonify(professores_db)
@@ -18,44 +37,28 @@ def professores():
 
 @app.route('/api/professores/<int:id>', methods=['GET'])
 def professorPorId(id):
-    for professor in professores_db:
-        if professor["id"] == id:
-            turmas = acharTurmas(professor['id'])
-     
-            return jsonify({
-                "Professor": professor,
-                "turmas ministradas": turmas,
-                "code": 200})
-                    
-            return  jsonify(professor)
+
     
-    return jsonify({'mensagem': 'Professor não encontrado'}), 404
+    return model.professorPorId(id)
 
 
 @app.route('/api/professores', methods=['POST'])
 def cria_professor():
-    professor = request.get_json()
-    professor['id'] = len(professores_db) + 1
-    professores_db.append(professor)
-    return jsonify(professor)
+
+    return model.cria_professor()
 
 
 @app.route('/api/professores/<int:id>', methods=['PUT'])
 def atualiza_professor(id):
-    professor = request.get_json()
-    for i in range(len(professores_db)):
-        if professores_db[i]['id'] == id:
-            professores_db[i].update(professor)
-            return jsonify(professores_db[i])
+
+           
+            return model.atualiza_professor(id)
 
 
 @app.route('/api/professores/<int:id>', methods=['DELETE'])
 def deleta_professor(id):
-    for i in range(len(professores_db)):
-        if professores_db[i]['id'] == id:
-            professores_db.pop(i)
-            return jsonify(professores_db)
+
+            return model.deleta_professor(id)
     
 if __name__ == '__main__':
     app.run(debug=True)
-
