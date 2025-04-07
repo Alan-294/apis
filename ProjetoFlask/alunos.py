7c8cb81915c6be2a312a7b98b42fa7d71fb9cae0
lirouana <ana.misquita@aluno.faculdade.com.br>
2025-04-07 16:34:55 -0300

    update alunos.py

--- ProjetoFlask/alunos.py
+++ ProjetoFlask/alunos.py
@@ -1,20 +1,35 @@
 from flask import Flask,jsonify, request
 import random
+import model_aluno as model
 from app import app
-from BD import alunos_db, turma_db
 
 def criar_id():
     novo_id = random.randint(1000, 9999)
     if not any(aluno["id"] == novo_id for aluno in alunos_db):  
         return novo_id  
    
+alunos_db = [
+     {
+        "id": 1009,
+        "nome": "João Pedro",
+        "data_nascimento": "2000-01-01",
+        "nota_primeiro_semestre": 10.0,
+        "nota_segundo_semestre": 8.0,
+        "turma_id": 6,
+        "mediaFinal": 9
+    },
+     {
+        "id": 3029,
+        "nome": "André Augusto",
+        "data_nascimento": "1998-09-05",
+        "nota_primeiro_semestre": 8.0,
+        "nota_segundo_semestre": 6.0,
+        "turma_id": 6,
+        "mediaFinal": 7
+    },
+]
 
-def turma_existe(turma_id):
-    #Verifica se o ID da turma existe na base de dados.
-    for turma in turma_db:
-        if (turma["id"] == turma_id):
-            return True
-    return False
+
 
 @app.route('/api/alunos', methods=['POST'])
 #Caminho do método POST 
@@ -29,20 +44,11 @@ def turma_existe(turma_id):
 #}
 #NÃO INSERIR O ID DO ALUNO JUNTO DO JSON
 
-    
 def cria_aluno():
 
     aluno = request.get_json()
-    
-    mediaFinal = (aluno['nota_primeiro_semestre'] + aluno['nota_segundo_semestre']) / 2
-    id_turma = aluno['turma_id']
-    if turma_existe(id_turma):
-        aluno['media_final'] = mediaFinal
-        aluno['id'] = criar_id()
         
-        alunos_db.append(aluno)
-        return jsonify(aluno)
-    return jsonify({"Erro": f"A turma {id_turma} não existe"})
+    return model.adiciona_aluno(aluno)
     
 
 #Caminho do método GET para todos os alunos
@@ -58,38 +64,19 @@ def consulta_alunos():
 #Exemplo de rota:
 #http://127.0.0.1:5000/api/alunos/1009
 def consulta_aluno(aluno_id):
-    for aluno in alunos_db:
-        if aluno["id"] == aluno_id:
-            return aluno
     
-    return jsonify({'mensagem': 'Usuário não encontrado'}), 404
+    return model.consulta_aluno(aluno_id)
 
 
 @app.route('/api/alunos/<int:aluno_id>', methods=['PUT'])
 def update_aluno(aluno_id):
-    for aluno in alunos_db:
-        if aluno['id'] == aluno_id:
-            novo_aluno = request.json
-        
-        
-            aluno['nome'] = novo_aluno.get('nome', aluno['nome'])
-            aluno['data_nascimento'] = novo_aluno.get('data_nascimento', aluno['data_nascimento'])
-            aluno['nota_primeiro_semestre'] = novo_aluno.get('nota_primeiro_semestre', aluno['nota_primeiro_semestre'])
-            aluno['nota_segundo_semestre'] = novo_aluno.get('nota_segundo_semestre', aluno['nota_segundo_semestre'])
-            aluno['turma_id'] = novo_aluno.get('turma_id', aluno['turma_id'])
-            aluno['media_final'] = novo_aluno.get('media_final', aluno['media_final'])
+
+    novo_aluno = request.json
             
-            return jsonify(aluno)
-    return jsonify({'mensagem': 'Usuário não encontrado'}), 404
+
+    return model.update_aluno(aluno_id)
 
 
 @app.route('/api/alunos/<int:aluno_id>', methods=['DELETE'])
 def deletar_aluno(aluno_id):
-    for aluno in alunos_db:
-        if aluno['id'] == aluno_id:
-            alunos_db.remove(aluno)
-            return jsonify({'mensagem': 'Usuário removido'})
-    return jsonify({'mensagem': 'Usuário não encontrado'}), 404
-
-
-
+    return model.deletar_aluno(aluno_id)
