e38f08ff8b06245d0e46ab7b9ddb55019f31a5c4
lirouana <ana.misquita@aluno.faculdade.com.br>
2025-04-07 16:47:52 -0300

    create model_turmas

---
+++ ProjetoFlask/models/model_turmas.py
@@ -0,0 +1,93 @@
+from flask import Flask,jsonify, request
+import random
+from app import app
+
+dados_turma = [
+     {
+    "id": 1,
+    "nome": "ADS3",
+    "turno": "manha",
+    "professor_id": 1,
+    "ativo": True
+  }
+]
+
+def gerar_id():
+    ids_existentes = {turma['id'] for turma in dados_turma}
+    
+    num = 0
+    while num in ids_existentes:
+        num += 1
+    
+    return num 
+
+def apiTurma():
+    metodo = request.method
+
+
+    if metodo == "GET": # http://127.0.0.1:5000/api/turma 
+        id_turma = request.args.get('id')
+        if id_turma:
+            for turma in dados_turma:
+                if turma['id'] == int(id_turma):
+                    return {"turma": turma,
+                            "code": 200}
+            return {"message": "Turma não encontrada",
+                 'code': 404}
+        return {"turmas": dados_turma,
+            "code": 200}
+    
+
+    
+    elif metodo == "POST": #http://127.0.0.1:5000/api/turma?nome=ads2&turno=tarde&professor_id=12345
+
+        nome = request.args.get('nome')  
+        turno = request.args.get('turno')  
+        professor_id = request.args.get('professor_id')
+
+
+        dados_turma.append(
+            {
+                "id": gerar_id(),
+                "nome": nome,
+                "turno": turno,
+                "professor_id": professor_id,
+                "ativo": True
+            }
+        )
+        return {"message": "Turma adicionada com sucesso",
+                'code': 200,
+                'turma_adicionada':  {
+                "id": dados_turma[len(dados_turma)-1],           
+            } }
+    
+    
+    elif metodo == "DELETE": #http://127.0.0.1:5000/api/turma?id=1
+        id_turma = request.args.get('id')
+        for turma in dados_turma:
+            if turma['id'] == int(id_turma):
+                dados_turma.remove(turma)
+                return {"message": "Turma excluída com sucesso",
+                        "code": 200}
+            
+    
+    elif metodo == "PUT": #http://127.0.0.1:5000/api/turma?id=1&nome=ads2 (alterado)&turno=Noite&professor_id=13
+        id_turma = request.args.get('id')
+        nome = request.args.get('nome')  
+        turno = request.args.get('turno')  
+        professor_id = request.args.get('professor_id')
+        posicao = -1
+        for turma in dados_turma:
+            posicao += 1
+            if turma['id'] == int(id_turma):
+                break               
+        dados_turma[posicao] = {
+            "id": int(id_turma),
+            "nome": nome,
+            "turno": turno,
+            "professor_id": professor_id,
+            "ativo": True
+        }
+        return {"message" : "Turma atualizada com sucesso",
+                "code": 200,
+               }
