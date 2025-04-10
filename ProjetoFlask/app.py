521786dd13a502ef402b471df618efd9ac8c1755
Fernanda Figueiredo <fernandafigueiredo962@gmail.com>
2025-04-10 02:33:24 -0300

    outras alteracoes no app.py

--- ProjetoFlask/app.py
+++ ProjetoFlask/app.py
@@ -1,4 +1,9 @@
 from flask import Flask,jsonify, request
+from Controllers.alunos_routes import alunos_blueprint
+from Controllers.professores_routes import professores_blueprint
+
+app.register_blueprint(alunos_blueprint)
+app.register_blueprint(professores_blueprint)
 
 app = Flask(__name__)
 from professores import *
