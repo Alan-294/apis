e57fd52ae99610ab1fbe3c9e04c7128f665e14ce
Alan Araujo <alanaraujosilveira6@gmail.com>
2025-05-03 21:07:54 -0300

    Criação do docker-composer para rodar o MySQL em docker

--- ProjetoFlask/app.py
+++ ProjetoFlask/app.py
@@ -2,6 +2,16 @@ from flask import Flask
 from controllers.alunos_controller import alunos_bp
 from controllers.professores_controller import professores_bp
 from controllers.turmas_controller import turmas_bp
+import os
+import mysql.connector
+
+conexao = mysql.connector.connect(
+    host=os.getenv('DB_HOST', 'localhost'),
+    port=int(os.getenv('DB_PORT', 3306)),
+    user=os.getenv('DB_USER', 'root'),
+    password=os.getenv('DB_PASSWORD', ''),
+    database=os.getenv('DB_NAME', 'banco_escolar')
+)
 
 app = Flask(__name__)
 
